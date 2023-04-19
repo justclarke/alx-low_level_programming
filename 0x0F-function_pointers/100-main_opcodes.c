295fb4e771baea2fddd3f24301aecc679db8b35f
justclarke <robertclarke29.rc@gmail.com>
2023-04-19 15:42:57 -0700

    update

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int bytes, a;
+	char *array;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	array = (char *)main;
+	for (a = 0; a < bytes; a++)
+	{
+		if (a == bytes - 1)
+		{
+			printf("%02hhx\n", array[a]);
+			break;
+		}
+		printf("%02hhx ", array[a]);
+	}
+	return (0);
+}
