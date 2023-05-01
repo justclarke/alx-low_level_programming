4e1318ac254bb35c3515194a8099b8ebd1467175
justclarke <robertclarke29.rc@gmail.com>
2023-05-01 13:19:05 -0700

    update

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_listint - frees a list
+ * @head: node
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *current, *new;
+
+	if(head == NULL)
+	{
+		return;
+	}
+	current = head;
+	while (current != NULL)
+	{
+		new = current->next;
+		free(current);
+		current = new;
+	}
+}
