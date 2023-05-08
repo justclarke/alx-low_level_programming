a2667535b995008ea0cffd1f4e0f55d1f6ea4a19
justclarke <robertclarke29.rc@gmail.com>
2023-05-08 14:39:26 -0700

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,6 +4,7 @@
  * and prints it to the POSIX standard output.
  * @filename: the file to be read
  * @letters: the number of letters it should read and print
+ *
  * Return: the actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
