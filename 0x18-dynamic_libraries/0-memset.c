53b010aa484ad8cc5c53019455c3012be276b06e
Robert Clarke <robertclarke29.rc@gmail.com>
2023-09-18 05:40:08 -0700

    added libdynamic.so

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - fills a specific value into the n byte of memory
+ * @n: number of bytes
+ * @b: bytes to be filled
+ * @s: starting address of memory
+ * Return: array with new value of the n bytes
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
---
+++ 0x18-dynamic_libraries/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strpbrk - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s != '\0')
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return (0);
+}
